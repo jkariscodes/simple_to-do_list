40a01828d52b86779f465a1260d605b2301321e0
Joseph Kariuki <joehene@gmail.com>
2021-07-15 09:46:27 +0300

    Created to-do list script.

---
+++ to_do_list.py
@@ -0,0 +1,15 @@
+"""
+/***************************************************************************
+Name                 : PyQt To Do List
+Description          : A simple to-do list application for your desktop.
+Date                 : 15/July/2021
+copyright            : (C) 2021 by Joseph Kariuki
+email                : joehene@gmail.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the stated license; or any later version.       *                             *
+ *                                                                         *
+ ***************************************************************************/
+"""
